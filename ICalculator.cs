25eb2565e41f2b66991d9585887dd99410fd8ebf
Doctor-GB <allahverdyanarko@gmail.com>
2026-05-19 09:18:53 +0300

    Update ICalculator.cs

--- ICalculator.cs
+++ ICalculator.cs
@@ -2,9 +2,3 @@ public interface ICalculator
 {
     decimal CalculateTotal(List<decimal> prices);
 }
-Реализуйте интерфейс в OrderCalculator:
-csharp
-public class OrderCalculator : ICalculator
-{
-    // код остаётся прежним
-}
